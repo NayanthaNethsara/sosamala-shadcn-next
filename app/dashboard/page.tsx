c9d40455f70ca8df0ef02b111768db92a04d4d51
Devin <104326559+devin1324@users.noreply.github.com>
2023-10-16 00:58:56 +0530

    bloom

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,35 +1,56 @@
 "use client"
 
-import Image from "next/image"
-
+import BloomsList from "@/components/custom/BloomsList"
 import EnvCard from "@/components/custom/EnvCard"
+import PlantDetailsChart from "@/components/custom/PlantDetailsChart"
 
 export default function DashboardExample() {
   return (
-    <main className="px-5">
+    <main className="flex flex-col gap-y-10 px-5">
       {/* Environment Variables */}
-      <div className="mt-10">
-        <h2>Environment Variables</h2>
+      <div className="mt-10 flex flex-col gap-y-5">
+        <h2 className="text-xl font-semibold">Environment Variables</h2>
 
-        <div className="mt-5 flex flex-wrap justify-between gap-y-6">
-          
-          <EnvCard iconSrc="/icons/drop.svg" sensorName="Humidity Percentage" percentage={72} />
+        <div className="flex flex-wrap justify-between gap-y-6">
+          <EnvCard
+            iconSrc="/icons/drop.svg"
+            sensorName="Humidity Percentage"
+            percentage={72}
+          />
 
-          <EnvCard iconSrc="/icons/drop.svg" sensorName="Humidity Percentage" percentage={72} />
+          <EnvCard
+            iconSrc="/icons/drop.svg"
+            sensorName="Humidity Percentage"
+            percentage={72}
+          />
 
-          <EnvCard iconSrc="/icons/drop.svg" sensorName="Humidity Percentage" percentage={72} />
+          <EnvCard
+            iconSrc="/icons/drop.svg"
+            sensorName="Humidity Percentage"
+            percentage={72}
+          />
 
-          <EnvCard iconSrc="/icons/drop.svg" sensorName="Humidity Percentage" percentage={72} />
-
+          <EnvCard
+            iconSrc="/icons/drop.svg"
+            sensorName="Humidity Percentage"
+            percentage={72}
+          />
         </div>
       </div>
 
+      {/* Plant Details */}
+      <div className="flex flex-col gap-y-5">
+        <h2 className="text-xl font-semibold">Plant Details</h2>
 
-        {/* Plant Details */}
+        <PlantDetailsChart />
+      </div>
 
-        
+      {/* Blooms */}
+      <div>
+        <h2 className="text-xl font-semibold">Blooms</h2>
 
-
+        <BloomsList />
+      </div>
     </main>
   )
 }
--- components/custom/BloomsList.tsx
+++ components/custom/BloomsList.tsx
@@ -1,6 +1,6 @@
 export default function BloomsList() {
     return (
-        <div>
+        <div >
         
         </div>
     )
