42f66d1b88761d23cf646a8876679996483b141a
Devin <104326559+devin1324@users.noreply.github.com>
2023-10-16 00:26:01 +0530

    Env component implemented

---
+++ components/custom/EnvCard.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image"
+
+export default function EnvCard({iconSrc, percentage, sensorName}:{
+    iconSrc: string,
+    percentage: number,
+    sensorName: string
+}) {
+  return (
+    <div className="flex aspect-square w-44 flex-col items-center justify-center gap-y-3 rounded-2xl bg-white p-3 [background:radial-gradient(50%_50%_at_50%_50%,rgba(255,255,255,0.4)_0%,rgba(255,255,255,0)_100%)]">
+      <Image
+        src={iconSrc}
+        alt=""
+        width={100}
+        height={100}
+        className="aspect-square w-14"
+      />
+      <p className="font-semibold">{percentage}%</p>
+
+      <div className="flex flex-row gap-1 justify-center items-center text-center">
+        <Image
+          alt=""
+          src="/icons/alert-circle.svg"
+          width={20}
+          height={20}
+          className="aspect-square w-4"
+        />
+        <p className="text-xs">{sensorName}</p>
+      </div>
+    </div>
+  )
+}
