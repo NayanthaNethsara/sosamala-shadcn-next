49f74caf58f01f6bd94f67f7dd31680c760ba6e6
Nayantha Nethsra <nayanthanethsara@gmail.com>
2023-10-18 20:28:45 +0530

    new file:   app/allinone/page.tsx deleted:    app/dashboard/page.tsx modified:   app/page.tsx modified:   components/SideBar.tsx modified:   components/main-nav.tsx

---
+++ app/allinone/page.tsx
@@ -0,0 +1,33 @@
+//@ts-nocheck
+"use client"
+
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+import { SensorData, StatusUpdate } from "@/components/firebase-update"
+
+export default function IndexPage() {
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Sosamala is a smart electronic system designed{" "}
+          <br className="hidden sm:inline" />
+          to monitor and care for rose plants.
+        </h1>
+
+        <br className="hidden sm:inline" />
+
+        <SensorData sensor="Humidity" />
+        <SensorData sensor="Moisture" />
+        <SensorData sensor="Temperature" />
+      </div>
+
+      <div className="flex gap-4">
+        <StatusUpdate dispenser="Dispenser" />
+        <StatusUpdate dispenser="MistingNozzle" />
+      </div>
+    </section>
+  )
+}
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -9,24 +9,21 @@ export default function SideBar() {
   return (
     <div className="flex flex-col pt-5">
       <SideBarThumbnails
-        href="/dashboard"
+        href="/"
         src="/icons/logo.svg"
         title="Sosamala"
         textstyle="text-xl font-semibold text-neutral-200"
       />
+
       <div className="flex flex-col pt-10 gap-8">
-        <SideBarThumbnails
-          href="/dashboard"
-          src="/icons/home.svg"
-          title="Dashboard"
-        />
+        <SideBarThumbnails href="/" src="/icons/home.svg" title="Dashboard" />
 
         <SideBarThumbnails
           href="/superior"
           src="/icons/superior.svg"
           title="Superior"
         />
-        <SideBarThumbnails href="/logs" src="/icons/logs.svg" title="Stats" />
+        <SideBarThumbnails href="/stats" src="/icons/logs.svg" title="Stats" />
 
         <SideBarThumbnails href="/team" src="/icons/team.svg" title="Team" />
       </div>
