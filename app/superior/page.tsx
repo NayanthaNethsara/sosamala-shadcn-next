d14bae7e8299a1f82c72d953a98fd29e6a57f490
Nayantha Nethsra <nayanthanethsara@gmail.com>
2023-10-20 23:00:52 +0530

    modified:   app/superior/page.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/superior/page.tsx
+++ app/superior/page.tsx
@@ -1,8 +1,9 @@
 // @ts-nocheck
-
 "use client"
 
-import { BellRing, Check } from "lucide-react"
+import React, { useEffect, useState } from "react"
+import { onValue, ref, set } from "firebase/database"
+import { Check, Shield } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
@@ -16,43 +17,55 @@ import {
 } from "@/components/ui/card"
 import { Switch } from "@/components/ui/switch"
 
+import { db } from "../firebase"
+
 const notifications = [
   {
-    title: "Your call has been confirmed.",
-    description: "1 hour ago",
+    title: "This mode allows manual control, but use it with caution",
   },
   {
-    title: "You have a new message!",
-    description: "1 hour ago",
-  },
-  {
-    title: "Your subscription is expiring soon!",
-    description: "2 hours ago",
+    title: "Remember to revert to automation when done",
   },
 ]
 
 type CardProps = React.ComponentProps<typeof Card>
 
 export default function Superior({ className, ...props }: CardProps) {
+  const [isSwitchOn, setIsSwitchOn] = useState(false)
+
+  const handleSwitchChange = (isChecked) => {
+    setIsSwitchOn(isChecked)
+
+    // Update the Firebase value based on the switch state
+    const SuperiorRef = ref(db, "/Sosamala/Superior/Mist")
+    set(SuperiorRef, isChecked)
+    const Soil = ref(db, "/Sosamala/Superior/Soil")
+    set(Soil, isChecked)
+  }
+
+  useEffect(() => {
+    // Listen for changes in the Firebase value and update the switch state
+    const SuperiorRef = ref(db, "/Sosamala/Superior/Mist")
+    onValue(SuperiorRef, (snapshot) => {
+      const value = snapshot.val()
+      setIsSwitchOn(value)
+    })
+  }, [])
+
   return (
     <div className="p-4">
-      <Card className={cn("w-[100%]", className)} {...props}>
+      <Card className={cn("w-[100%] lg:w-[400px ]", className)} {...props}>
         <CardHeader>
           <CardTitle>Command and Conquer</CardTitle>
-          <CardDescription>Your Website, Your Decisions.</CardDescription>
+          <CardDescription> Take control into your own hands.</CardDescription>
         </CardHeader>
         <CardContent className="grid gap-4">
           <div className=" flex items-center space-x-4 rounded-md border p-4">
-            <BellRing />
+            <Shield />
             <div className="flex-1 space-y-1">
-              <p className="text-sm font-medium leading-none">
-                Push Notifications
-              </p>
-              <p className="text-sm text-muted-foreground">
-                Send notifications to device.
-              </p>
+              <p className="text-sm font-medium leading-none">Superior Mode</p>
             </div>
-            <Switch />
+            <Switch onCheckedChange={handleSwitchChange} checked={isSwitchOn} />
           </div>
           <div>
             {notifications.map((notification, index) => (
@@ -62,12 +75,10 @@ export default function Superior({ className, ...props }: CardProps) {
               >
                 <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
                 <div className="space-y-1">
-                  <p className="text-sm font-medium leading-none">
+                  <p className="text-sm font-regular leading-none ">
                     {notification.title}
                   </p>
-                  <p className="text-sm text-muted-foreground">
-                    {notification.description}
-                  </p>
+                  {!isSwitchOn ? null : <></>}
                 </div>
               </div>
             ))}
