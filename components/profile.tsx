dcbf10f64b8baa15abf0d07aea7ababf63e93edf
Nayantha Nethsra <nayanthanethsara@gmail.com>
2023-10-22 22:52:44 +0530

    modified:   components/profile.tsx 	modified:   components/ui/avatar.tsx

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,4 +1,5 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import * as Avatar from "@radix-ui/react-avatar"
+
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -22,11 +23,19 @@ export default function Profile({
     <Card>
       <CardHeader>
         <div className="flex w-full gap-5 grid-cols-2">
-          <Avatar size="100px">
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>{name}</AvatarFallback>
-          </Avatar>
-
+          <Avatar.Root className="bg-blackA1 inline-flex h-[100px] w-12 select-none items-center justify-center overflow-hidden rounded-full align-middle">
+            <Avatar.Image
+              className="h-full w-full rounded-[inherit] object-cover"
+              src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+              alt="Colm Tuite"
+            />
+            <Avatar.Fallback
+              className="text-violet11 leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium"
+              delayMs={600}
+            >
+              CT
+            </Avatar.Fallback>
+          </Avatar.Root>
           <div className="space-y-1">
             <CardTitle>
               <p className="text-xl font-bold leading-tight tracking-tighter">
